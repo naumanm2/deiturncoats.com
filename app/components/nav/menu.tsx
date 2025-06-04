0955029e1341ef486ebe03e060f46302a88edb0b
@max.naumanen <naummaxc@gmail.com>
2025-06-04 15:04:49 +0300

    fixes

--- app/components/nav/menu.tsx
+++ app/components/nav/menu.tsx
@@ -11,20 +11,20 @@ export const Menu = ({ open, setOpen, locale }: tMenu) => {
     <>
       {open && (
         <div className="w-full flex-1 h-dvh fixed top-0 z-40 p-2 bg-background">
-          <div className="flex flex-col h-full justify-end items-start gap-2 pb-64">
+          <div className="flex flex-col h-full justify-end items-start gap-2 pb-48">
             <Link
               href={`/${locale}`}
               onClick={() => setOpen(!open)}
               className="font-bold text-4xl underline p-2 hover:opacity-80 transition-opacity duration-200 ease-in-out uppercase"
             >
-              Home
+              {locale == "fi" ? "Koti": "Home"}
             </Link>
             <Link
               href={locale == "fi" ? `/${locale}/about` : `/${locale}/about`}
               onClick={() => setOpen(!open)}
               className="font-bold text-4xl underline p-2 hover:underline hover:opacity-80 transition-opacity duration-200 ease-in-out uppercase"
             >
-              About
+              {locale == "fi" ? "Meistä": "About"}
             </Link>
           </div>
         </div>
